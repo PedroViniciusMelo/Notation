5aa7fac25b7a51da87fa5b57deeed25c98d7d502
Pedro Vinícius de Melo Silva <PedroViniciusMelo@users.noreply.github.com>
2020-11-05 14:56:08 -0300

    Revert "Initial commit"
    This reverts commit a496fdcbd8b08ca9129c84fa6a665aae60ed1633.

--- App.js
+++ App.js
@@ -1,24 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {useFonts} from 'expo-font';
-import {NavigationContainer} from "@react-navigation/native";
-import {Poppins_600SemiBold, Poppins_700Bold, Poppins_400Regular, Poppins_800ExtraBold} from '@expo-google-fonts/poppins';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Poppins_600SemiBold,
-    Poppins_700Bold,
-    Poppins_400Regular,
-    Poppins_800ExtraBold
-  });
-  if (!fontsLoaded){
-    return null;
-  }
-
-
   return (
-    <NavigationContainer>
-
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
-};
+}
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
