c9258beb9c575879a7638c01abf5344c58e3e6e5
Pedro Vinícius de Melo Silva <PedroViniciusMelo@users.noreply.github.com>
2020-11-06 14:03:18 -0300

    Add custom drawer

--- App.js
+++ App.js
@@ -6,7 +6,11 @@ import SplashScreen from './src/SplashScreen/Index';
 import NewActivity from './src/NewActivity/Index';
 import Home from  './src/Home/Index';
 import ViewActivity from  './src/ViewActivity/Index';
-import {createDrawerNavigator} from "@react-navigation/drawer";
+import {createDrawerNavigator, DrawerContentScrollView, DrawerItem} from "@react-navigation/drawer";
+import {View, Text, StyleSheet} from "react-native";
+import DrawerSection from "react-native-paper/src/components/Drawer/DrawerSection";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -38,21 +42,92 @@ export default function App() {
         <ViewActivity/>
     )
   }
+  function CustomDrawerNavigator(props){
+    return(
+        <View style={{flex: 1}}>
+          <DrawerContentScrollView {...props} style={{backgroundColor: '#0085FF'}}>
+            <DrawerSection>
+                <View style={{justifyContent: 'center', alignItems: 'center', borderRadius: 20, elevation: 10, backgroundColor: 'white', alignSelf: 'center', width: '80%'}}>
+                    <Text style={{fontFamily: 'Poppins_700Bold', fontSize: 60}}>N<Text style={{fontSize: 40}}>otation</Text></Text>
+                </View>
+            </DrawerSection>
+            <DrawerSection {...props}>
+              <DrawerItem
+                  label={'Atividades'}
+                  icon={({color, size}) => (
+                      <Icon
+                          name='ballot-outline'
+                          color={color}
+                          size={size}
+                          />
+                  )}
+                  onPress={()=>{props.navigation.navigate('Atividades')}}
+                  style={styles.divisoes}
+              />
+              <DrawerItem
+                  label={'Calendário'}
+                  icon={({color, size}) => (
+                      <Icon
+                          name='calendar'
+                          color={color}
+                          size={size}
+                      />
+                  )}
+                  onPress={()=>{props.navigation.navigate('Calendário')}}
+                  style={styles.divisoes}
+              />
+              <DrawerItem
+                  label={'Histórico'}
+                  icon={({color, size}) => (
+                      <Icon
+                          name='history'
+                          color={color}
+                          size={size}
+                      />
+                  )}
+                  onPress={()=>{props.navigation.navigate('Histórico')}}
+                  style={styles.divisoes}
+              />
+              <DrawerItem
+                  label={'Sobre o App'}
+                  icon={({color, size}) => (
+                      <Icon
+                          name='developer-board'
+                          color={color}
+                          size={size}
+                      />
+                  )}
+                  onPress={()=>{props.navigation.navigate('Sobre o App')}}
+                  style={styles.divisoes}
+              />
+            </DrawerSection>
+          </DrawerContentScrollView>
+        </View>
+        )
+  }
 
   const SideBar = createDrawerNavigator();
+  const styles = StyleSheet.create({
+      divisoes : {
+          backgroundColor: 'white',
+          borderRadius: 20,
+          fontSize: 40,
+          marginTop: 10
+      }
+
+  })
 
   function DrawerNavigator(){
     return(
-        <SideBar.Navigator>
-          <SideBar.Screen name={'Home'} component={Inicio}/>
-          <SideBar.Screen name={'Atividades'} component={Cadastro}/>
-          <SideBar.Screen name={'Todas as tarefas'} component={Principal}/>
-          <SideBar.Screen name={'Ver atividades'} component={tarefas}/>
+        <SideBar.Navigator drawerContent={(props) => <CustomDrawerNavigator {...props}/>}>
+          <SideBar.Screen name={'Atividades'} component={Inicio}/>
+          <SideBar.Screen name={'Calendário'} component={Cadastro}/>
+          <SideBar.Screen name={'Histórico'} component={Principal}/>
+          <SideBar.Screen name={'Sobre o App'} component={tarefas}/>
         </SideBar.Navigator>
     )
 
   }
-
   return (
     <NavigationContainer>
       <DrawerNavigator/>
